3c37514045b1614d2b0f4d47aea4fca9d476cddd
Dan <danwan754@gmail.com>
2018-08-22 18:15:37 -0700

    Link news story to source

--- src/NewsResult.js
+++ src/NewsResult.js
@@ -4,18 +4,19 @@ class NewsResult extends Component {
 
   render() {
     if (Object.keys(this.props.newsArray).length > 0) {
-      // console.log(new Date(this.props.newsArray[0].datetime).toLocaleString());
       return (
         <div>
         { this.props.newsArray.map(news =>
           { return (
-            <div key={news.headline}>
+            <a key={news.headline} href={news.url}>
+            <span style={{display: "block"}}>
               <h2>{news.headline}</h2>
               <h3>{news.summary}</h3>
               <h3>{news.image}</h3>
               <h3>{news.url}</h3>
               <h4>{new Date(news.datetime).toLocaleString()}</h4>
-            </div>
+            </span>
+            </a>
           )}
         )}
         </div>
