bc6127e97e44bdd8ffb040eb5407d6bd948f69e9
Dan <danwan754@gmail.com>
2018-09-28 00:14:07 -0700

    Change paragraph in Home page

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,12 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h2>Welcome</h2>
+      <div className="outter-div">
         <br/>
-        <p>This web-app will allow you to quote, make watch lists, view
+        <p id="home-intro-p">Quote stocks, make watch lists, view
         analyst ratings, and read latest news that may affect your stocks.</p>
-        <br/>
-        <p>Companies that are available to quote are U.S listed companies that are listed on <a href="https://iextrading.com/" target="_blank">IEX</a>.</p>
+        <br/><br/><br/>
+        <p id="home-notice-p">Companies that are available to quote are U.S listed companies that are listed on <a href="https://iextrading.com/" target="_blank">IEX</a>.</p>
       </div>
     );
   }
