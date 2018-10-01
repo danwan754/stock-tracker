34729cbdcadaebdbe699e0d685bae4755733f1a0
Dan <danwan754@gmail.com>
2018-09-30 17:39:40 -0700

    Fix bug where server not serving correct files

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, './client/dist')));
 
 // app.get('/ping', function (req, res) {
 //  return res.send('pong');
 // });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, './client/dist', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
