67d8ace599b1c6931a6d957487f4b733d0d97373
Dan <danwan754@gmail.com>
2018-10-01 23:35:34 -0700

    Remove content out of client dir

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-var request = require('request');
-
+const request = require('request');
+// const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, './client/dist')));
+app.use(express.static(path.join(__dirname, './dist')));
 app.use(bodyParser.json());
+// app.use(cors());
 
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  next()
-})
+// app.all('*', function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+//   res.header('Access-Control-Allow-Headers', 'Content-Type')
+//   next()
+// })
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, './dist', 'index.html'));
 });
 
 
